94f0888857b9b056dadc5af2d2d0ccece694a7b2
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2019-03-21 05:15:05 +0000

    complete double-quote problem. (haven't tested yet.)

--- csv-parse-and-stringiy.js
+++ csv-parse-and-stringiy.js
@@ -2,7 +2,6 @@ const CSV = class {
   static parse(string) {
     const rtnArray = new Array();
     const first_split = string.replace(/\r\n/g,"\n").replace(/\r/g,"\n").split("\n"); // <- Split without thinking anything :-)
-
     const marge_status = {
       marge: false,
       row: -1,
@@ -11,28 +10,25 @@ const CSV = class {
 
     for(let i=0; i<first_split.length; i++) {
       rtnArray[i] = first_split[i].split(","); // <- Split without thinking anything :-)
-
       for(let j=0; j<rtnArray[i].length; j++) { // Let's check and modify each cells.
         let cell_value = rtnArray[i][j];
-
         if(!marge_status.marge && cell_value[0]==="\"") { // Meaning the start of a Wonderful-Cell.
           marge_status.marge = true;
           marge_status.row = i;
           marge_status.colmun = j;
-
-          rtnArray[i][j] = cell_value.slice(1); // <- Remove "\"" from the head of the value.
+          rtnArray[i][j] = cell_value.slice(1).replace(/""/g,"\""); // <- Remove \" from the head of the value and replace \"\" to \".
         }
         if(marge_status.marge) { // Meaning [in / the end of] a Wonderful-Cell.
           if(cell_value[cell_value.length-1]==="\"") { // Meaning the end of a Wonderful-Cell.
             marge_status.marge = false;
             cell_value = cell_value.slice(0,-1); // <- Remove "\"" from the end of the value.
           }
-          rtnArray[marge_status.row][marge_status.colmun] += (marge_status.row===i ? ",":"\r\n") + cell_value; // <- Now, marge and...
+          cell_value = (marge_status.row===i ? ",":"\r\n") + cell_value.replace(/""/g,"\""); // Restorate separator and replace \"\" to \".
+          rtnArray[marge_status.row][marge_status.colmun] += cell_value; // <- Now, marge and...
           rtnArray[i].splice(j,1); // <- remove incollect cell.
         }
       }
     }
-
     return rtnArray;
   }
 
@@ -41,14 +37,13 @@ const CSV = class {
       for(let j=0; j<array[i].length; j++) {
         const cell_value = array[i][j];
         const includesNewLine = cell_value.indexOf("\r\n")>-1 || cell_value.indexOf("\r")>-1 || cell_value.indexOf("\n")>-1;
-        const includesComma = cell.indexOf(",")>-1;
-        if(includesNewLine || includesComma) {
-          array[i][j] = `"${cell_value}"`;
-        }
+        const includesComma = cell_value.indexOf(",")>-1;
+        const includesQuote = cell_value.indexOf("\"")>-1;
+        if(includesQuote) cell_value = cell_value.replace(/"/g,"\"\"");
+        if(includesNewLine || includesComma || includesQuote) array[i][j] = `"${cell_value}"`;
       }
       array[i] = array[i].join(",");
     }
-
-    return array.join("\r\n");
+    return array.join(option.rowSeparator || "\r\n");
   }
 };
