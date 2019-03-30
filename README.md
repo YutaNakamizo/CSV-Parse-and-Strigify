8f4c0c96fd713c61784b6829305e8c9343a980c8
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2019-03-30 00:45:11 +0000

    Add type check, resolve error when return+" cell.

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# CSV-Parse-and-Strigify
+table# CSV-Parse-and-Strigify
 
 ## Quick Start
 `CSV.parse(string)` can be used for convert a csv string to a 2-dimensional array.
 ```javascript
 // const csv <- CSV String that was read from <input type="file"> and File API.
 
-const arr = CSV.parse(csv);
+const table = CSV.parse(csv);
 ```
 
 `CSV.stringify(array)` can be used for convert a 2-dimensional array to a csv string.
 ```javascript
 // const arr <- 2-dimensional Array that was created in your program or was received from a server such as Google Apps Script.
 
-const csv = CSV.stringify(arr);
+const csv = CSV.stringify(table);
 ```
 
 
--- csv-parse-and-stringify.js
+++ csv-parse-and-stringify.js
@@ -1,5 +1,10 @@
 const CSV = class {
   static parse(string) {
+    if(typeof string !== "string") {
+      console.error("Type Error: The argument you set isn't string.");
+      return [[]];
+    }
+
     const rtnArray = string.replace(/\r\n/g,"\n").replace(/\r/g,"\n").split("\n"); // <- Split without thinking anything :-)
     const marge_status = {
       marge: false,
@@ -28,7 +33,7 @@ const CSV = class {
         else if(marge_status.marge) { // Meaning [in / the end of] a Wonderful-Cell.
           cell_value = cell_value.replace(/"{2}/g,"\""); // Restorate separator and replace \"\" to \".
           //console.log(cell_value);
-          if(cell_value[cell_value.length-1]==="\"" && cell_value[cell_value.length-2]!=="\"") { // Meaning the end of a Wonderful-Cell.
+          if(cell_value[cell_value.length-1]==="\"") { // Meaning the end of a Wonderful-Cell.
             marge_status.marge = false;
             cell_value = cell_value.slice(0,-1); // <- Remove "\"" from the end of the value.
           }
@@ -37,16 +42,25 @@ const CSV = class {
         }
       }
     }
-    for(let i=0; i<rtnArray.length; i++) if(rtnArray[i].length===0) rtnArray.splice(i,1);
+    for(let i=0; i<rtnArray.length; i++) if(rtnArray[i].length===0) rtnArray.splice(i--,1);
     return rtnArray;
   }
 
   static stringify(array, option) {
+    if(!Array.isArray(array)) {
+      console.error("Type Error: The argument you set isn't array.");
+      return "";
+    }
+
     const rtnCSV = new Array();
     for(let i=0; i<array.length; i++) {
+      if(!Array.isArray(array[i])) {
+        console.error("Type Error: A row of the argument you set isn't array.");
+        return "";
+      }
       rtnCSV[i] = new Array();
       for(let j=0; j<array[i].length; j++) {
-        let cell_value = array[i][j];
+        let cell_value = array[i][j] || ""; // if array[i][j] is undefined, set empty string.
         const includesNewLine = cell_value.indexOf("\r\n")>-1 || cell_value.indexOf("\r")>-1 || cell_value.indexOf("\n")>-1;
         const includesComma = cell_value.indexOf(",")>-1;
         const includesQuote = cell_value.indexOf("\"")>-1;
