9b804729d5c2fddd7f1dfa711af2fcac7b7ad9ec
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2019-10-24 10:31:54 +0900

    updated some code

--- csv-parse-and-stringify.js
+++ csv-parse-and-stringify.js
@@ -1,69 +1,58 @@
 const CSV = class {
   static parse(string) {
-    if(typeof string !== "string") {
-      console.error("Type Error: The argument you set isn't string.");
-      return [[]];
-    }
+    if(typeof string !== "string") throw new TypeError("The argument you set isn't a string.");
 
     const rtnArray = string.replace(/\r\n/g,"\n").replace(/\r/g,"\n").split("\n"); // <- Split without thinking anything :-)
-    const marge_status = {
-      marge: false,
+    const merge_status = {
+      merge: false,
       row: -1,
-      colmun: -1
+      column: -1
     };
 
-    for(let i=0; i<rtnArray.length; i++) {
+    for(let i in rtnArray) {
       rtnArray[i] = rtnArray[i].split(","); // <- Split without thinking anything :-)
-      for(let j=0; j<rtnArray[i].length; j++) { // Let's check and modify each cells.
+      for(let j in rtnArray[i]) { // Let's check and modify each cells.
         let cell_value = rtnArray[i][j];
         if(i===rtnArray.length-1 && j===0 && cell_value==="") { // <- break; when this cell is [EOF].
           rtnArray.splice(i,1);
           break;
         }
-        if(!marge_status.marge && cell_value[0]==="\"") { // Meaning the start of a Wonderful-Cell.
+        if(!merge_status.merge && cell_value[0]==="\"") { // Meaning the start of a Wonderful-Cell.
           if(cell_value[cell_value.length-1]!=="\"") {
-            marge_status.marge = true;
-            marge_status.row = i;
-            marge_status.colmun = j;
+            merge_status = {merge:true, row:i, column:j};
             cell_value = cell_value.slice(1);
           }
           else cell_value = cell_value.slice(1,-1);
           rtnArray[i][j] = cell_value.replace(/"{2}/g,"\""); // <- Remove \" from the head of the value and replace \"\" to \".
         }
-        else if(marge_status.marge) { // Meaning [in / the end of] a Wonderful-Cell.
+        else if(merge_status.merge) { // Meaning [in / the end of] a Wonderful-Cell.
           cell_value = cell_value.replace(/"{2}/g,"\""); // Restorate separator and replace \"\" to \".
           //console.log(cell_value);
           if(cell_value[cell_value.length-1]==="\"") { // Meaning the end of a Wonderful-Cell.
-            marge_status.marge = false;
+            merge_status.merge = false;
             cell_value = cell_value.slice(0,-1); // <- Remove "\"" from the end of the value.
           }
-          rtnArray[marge_status.row][marge_status.colmun] += (marge_status.row===i ? ",":"\r\n") + cell_value; // <- Now, marge.
+          rtnArray[merge_status.row][merge_status.column] += (merge_status.row===i ? ",":"\n") + cell_value; // <- Now, marge.
           rtnArray[i].splice(j--,1);
-          if(marge_status.row!==i) {
-            rtnArray[marge_status.row] = rtnArray[marge_status.row].concat(rtnArray[i]);
+          if(merge_status.row!==i) {
+            rtnArray[merge_status.row] = rtnArray[merge_status.row].concat(rtnArray[i]);
             rtnArray[i] = [];
           }
         }
       }
     }
-    for(let i=0; i<rtnArray.length; i++) if(rtnArray[i].length===0) rtnArray.splice(i--,1);
+    for(let i in rtnArray) if(rtnArray[i].length===0) rtnArray.splice(i--,1);
     return rtnArray;
   }
 
   static stringify(array) {
-    if(!Array.isArray(array)) {
-      console.error("Type Error: The argument you set isn't array.");
-      return "";
-    }
+    if(!Array.isArray(array)) throw new TypeError("The argument you set isn't an array.");
 
     const rtnCSV = new Array();
-    for(let i=0; i<array.length; i++) {
-      if(!Array.isArray(array[i])) {
-        console.error("Type Error: A row of the argument you set isn't array.");
-        return "";
-      }
+    for(let i in array) {
+      if(!Array.isArray(array[i])) throw new TypeError("A row of the argument you set isn't an array.");
       rtnCSV[i] = new Array();
-      for(let j=0; j<array[i].length; j++) {
+      for(let j in array[i]) {
         let cell_value = array[i][j] || ""; // if array[i][j] is undefined, set empty string.
         const includesNewLine = cell_value.indexOf("\r\n")>-1 || cell_value.indexOf("\r")>-1 || cell_value.indexOf("\n")>-1;
         const includesComma = cell_value.indexOf(",")>-1;
@@ -73,6 +62,6 @@ const CSV = class {
       }
       rtnCSV[i] = rtnCSV[i].join(",");
     }
-    return rtnCSV.join("\r\n");
+    return rtnCSV.join("\n");
   }
 };
